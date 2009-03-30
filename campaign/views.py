2f5ea9f1ba128017326ffc47cf741913b9a8e32f
Arne Brodowski <arne@rcs4u.de>
2009-03-30 12:20:43 +0000

    fix: only show campaigns online which are marked as 'online'
    git-svn-id: https://django-campaign.googlecode.com/svn/trunk@13 e421d7ae-feb3-11dd-a3ef-1f8437fb274a

--- campaign/views.py
+++ campaign/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from campaign.models import Campaign
 
 def view_online(request, object_id):
-    campaign = get_object_or_404(Campaign, pk=object_id)
+    campaign = get_object_or_404(Campaign, pk=object_id, online=True)
     
     if campaign.template.html is not None and campaign.template.html != u"":
         tpl = template.Template(campaign.template.html)
