2d2419d8d37db6109c6294ee42c3ca9dc46be1be
Arne Brodowski <arne@rcs4u.de>
2009-05-08 10:18:59 +0000

    bumped version to 0.1.1 after if f*cked up the (setup.py for the) 0.1 release.
    git-svn-id: https://django-campaign.googlecode.com/svn/trunk@21 e421d7ae-feb3-11dd-a3ef-1f8437fb274a

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='django-campaign',
     version=__import__('campaign').__version__,
     description='A basic newsletter app for the Django webframework',
+    long_description=open('docs/overview.txt').read(),
     author='Arne Brodowski',
     author_email='arne@rcs4u.de',
     url='http://code.google.com/p/django-campaign/',
@@ -19,4 +20,9 @@ setup(
     packages = (
         'campaign',
     ),
+    package_data = {
+        'campaign': 
+            ['templates/admin/campaign/campaign/*.html',
+             'templates/admin/campaign/subscriber/*.html'],
+    }
 )
