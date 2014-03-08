cf634fc7fa7cb90afb8880b2e1e5b35f58dd864a
Arne Brodowski <mail@arnebrodowski.de>
2014-03-08 19:25:29 +0100

    add an AppConfig class for Django 1.7+

--- campaign/__init__.py
+++ campaign/__init__.py
@@ -1,1 +1,3 @@
-__version__ = '0.2.0'
+__version__ = '0.3.0-pre'
+
+default_app_config = 'campaign.apps.CampaignConfig'
---
+++ campaign/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+class CampaignConfig(AppConfig):
+    name = 'campaign'
+    verbose_name = _("campaign management")
