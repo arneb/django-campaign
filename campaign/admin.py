ee587135f880181c87071e20866af90eef977d43
Arne Brodowski <mail@arnebrodowski.de>
2014-03-08 19:26:30 +0100

    django.utils.simplejson no longer exists in Django 1.7+

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render_to_response
 from django import template
 from django import forms
-from django.utils.functional import update_wrapper
+try:
+    from django.utils.functional import update_wrapper
+except ImportError:
+    from functools import update_wrapper
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.contrib.admin.util import unquote
@@ -12,28 +15,28 @@ from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 from campaign.models import MailTemplate, Campaign, BlacklistEntry, \
 BounceEntry, SubscriberList, Newsletter
- 
+
 
 class CampaignAdmin(admin.ModelAdmin):
     filter_horizontal=('recipients',)
     list_display=('name', 'newsletter', 'sent', 'sent_at', 'online')
     send_template = None
-    
+
     def has_send_permission(self, request, obj):
         """
         Subclasses may override this and implement more granular permissions.
         TODO: integrate with django's permisson system
         """
         return request.user.is_superuser
-        
-        
+
+
     def send_view(self, request, object_id, extra_context=None):
         """
         Allows the admin to send out the mails for this campaign.
         """
         model = self.model
         opts = model._meta
-        
+
         try:
             obj = model._default_manager.get(pk=unquote(object_id))
         except model.DoesNotExist:
@@ -41,27 +44,27 @@ class CampaignAdmin(admin.ModelAdmin):
             # permissions yet. We don't want an unauthenticated user to be able
             # to determine whether a given object exists.
             obj = None
-        
+
         if not self.has_send_permission(request, obj):
             raise PermissionDenied
-        
+
         if obj is None:
             raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
-        
+
         if request.method == 'POST':
             if not request.POST.get('send', None) == '1':
                 raise PermissionDenied
-            
+
             num_sent = obj.send()
             messages.success(request, _(u'The %(name)s "%(obj)s" was successfully sent. %(num_sent)s messages delivered.' %  {'name': force_unicode(opts.verbose_name), 'obj': force_unicode(obj), 'num_sent': num_sent,}))
             return HttpResponseRedirect('../')
 
-        
+
         def form_media():
             from django.conf import settings
             css = ['css/forms.css',]
             return forms.Media(css={'screen': ['%sadmin/%s' % (settings.STATIC_URL, url) for url in css]})
-        
+
         media = self.media + form_media()
 
         context = {
@@ -74,13 +77,13 @@ class CampaignAdmin(admin.ModelAdmin):
             'opts': opts,
         }
         context.update(extra_context or {})
-        
+
         return render_to_response(self.send_template or
             ['admin/%s/%s/send_object.html' % (opts.app_label, opts.object_name.lower()),
             'admin/%s/send_object.html' % opts.app_label,
             'admin/send_object.html'], context, context_instance=template.RequestContext(request))
-    
-        
+
+
     def get_urls(self):
         from django.conf.urls import patterns, url
 
@@ -90,7 +93,7 @@ class CampaignAdmin(admin.ModelAdmin):
             return update_wrapper(wrapper, view)
 
         info = self.admin_site.name, self.model._meta.app_label, self.model._meta.module_name
-        
+
         super_urlpatterns = super(CampaignAdmin, self).get_urls()
         urlpatterns = patterns('',
             url(r'^(.+)/send/$',
@@ -98,7 +101,7 @@ class CampaignAdmin(admin.ModelAdmin):
                 name='%sadmin_%s_%s_send' % info),
         )
         urlpatterns += super_urlpatterns
-        
+
         return urlpatterns
 
 
--- campaign/fields.py
+++ campaign/fields.py
@@ -1,45 +1,49 @@
 from django.conf import settings
 from django.db import models
 from django import forms
-from django.utils import simplejson
- 
+try:
+    from django.utils import simplejson
+except ImportError:
+    import json as simplejson
+
+
 class JSONWidget(forms.Textarea):
     def render(self, name, value, attrs=None):
         if not isinstance(value, basestring):
             value = simplejson.dumps(value, indent=2)
         return super(JSONWidget, self).render(name, value, attrs)
- 
+
 class JSONFormField(forms.CharField):
     def __init__(self, *args, **kwargs):
         kwargs['widget'] = JSONWidget
         super(JSONFormField, self).__init__(*args, **kwargs)
- 
+
     def clean(self, value):
         if not value: return
         try:
             return simplejson.loads(value)
         except Exception, exc:
             raise forms.ValidationError(u'JSON decode error: %s' % (unicode(exc),))
- 
+
 class JSONField(models.TextField):
     __metaclass__ = models.SubfieldBase
- 
+
     def formfield(self, **kwargs):
         return super(JSONField, self).formfield(form_class=JSONFormField, **kwargs)
- 
+
     def to_python(self, value):
         if isinstance(value, basestring):
             value = simplejson.loads(value)
         return value
- 
+
     def get_db_prep_save(self, value, connection=None):
         if value is None: return
         return simplejson.dumps(value)
- 
+
     def value_to_string(self, obj):
         value = self._get_val_from_obj(obj)
         return self.get_db_prep_value(value)
 
 if 'south' in settings.INSTALLED_APPS:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^campaign\.fields\.JSONField"])        
+    add_introspection_rules([], ["^campaign\.fields\.JSONField"])
