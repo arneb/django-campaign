0a765b0781aec8d110a4c514a360a7af420efb19
Arne Brodowski <mail@arnebrodowski.de>
2014-03-08 19:55:07 +0100

    switch migrations from South to native Django (1.7+) migrations

--- campaign/migrations/0001_initial.py
+++ campaign/migrations/0001_initial.py
@@ -1,133 +1,107 @@
-# -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# encoding: utf8
+from django.db import models, migrations
+import django.utils.timezone
+import campaign.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'MailTemplate'
-        db.create_table('campaign_mailtemplate', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('plain', self.gf('django.db.models.fields.TextField')()),
-            ('html', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('subject', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal('campaign', ['MailTemplate'])
+    dependencies = [
+        (u'contenttypes', '__first__'),
+    ]
 
-        # Adding model 'SubscriberList'
-        db.create_table('campaign_subscriberlist', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('filter_condition', self.gf('campaign.fields.JSONField')(default='{}')),
-            ('email_field_name', self.gf('django.db.models.fields.CharField')(max_length=64)),
-        ))
-        db.send_create_signal('campaign', ['SubscriberList'])
-
-        # Adding model 'Campaign'
-        db.create_table('campaign_campaign', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('template', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['campaign.MailTemplate'])),
-            ('sent', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('online', self.gf('django.db.models.fields.BooleanField')(default=True)),
-        ))
-        db.send_create_signal('campaign', ['Campaign'])
-
-        # Adding M2M table for field recipients on 'Campaign'
-        db.create_table('campaign_campaign_recipients', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('campaign', models.ForeignKey(orm['campaign.campaign'], null=False)),
-            ('subscriberlist', models.ForeignKey(orm['campaign.subscriberlist'], null=False))
-        ))
-        db.create_unique('campaign_campaign_recipients', ['campaign_id', 'subscriberlist_id'])
-
-        # Adding model 'BlacklistEntry'
-        db.create_table('campaign_blacklistentry', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-        ))
-        db.send_create_signal('campaign', ['BlacklistEntry'])
-
-        # Adding model 'BounceEntry'
-        db.create_table('campaign_bounceentry', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('email', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('exception', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-        ))
-        db.send_create_signal('campaign', ['BounceEntry'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'MailTemplate'
-        db.delete_table('campaign_mailtemplate')
-
-        # Deleting model 'SubscriberList'
-        db.delete_table('campaign_subscriberlist')
-
-        # Deleting model 'Campaign'
-        db.delete_table('campaign_campaign')
-
-        # Removing M2M table for field recipients on 'Campaign'
-        db.delete_table('campaign_campaign_recipients')
-
-        # Deleting model 'BlacklistEntry'
-        db.delete_table('campaign_blacklistentry')
-
-        # Deleting model 'BounceEntry'
-        db.delete_table('campaign_bounceentry')
-
-
-    models = {
-        'campaign.blacklistentry': {
-            'Meta': {'ordering': "('-added',)", 'object_name': 'BlacklistEntry'},
-            'added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        },
-        'campaign.bounceentry': {
-            'Meta': {'ordering': "('email',)", 'object_name': 'BounceEntry'},
-            'email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'exception': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        },
-        'campaign.campaign': {
-            'Meta': {'ordering': "('name', 'sent')", 'object_name': 'Campaign'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'online': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'recipients': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['campaign.SubscriberList']", 'symmetrical': 'False'}),
-            'sent': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'template': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['campaign.MailTemplate']"})
-        },
-        'campaign.mailtemplate': {
-            'Meta': {'ordering': "('name',)", 'object_name': 'MailTemplate'},
-            'html': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'plain': ('django.db.models.fields.TextField', [], {}),
-            'subject': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        'campaign.subscriberlist': {
-            'Meta': {'ordering': "('name',)", 'object_name': 'SubscriberList'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'email_field_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'filter_condition': ('campaign.fields.JSONField', [], {'default': "'{}'"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['campaign']
+    operations = [
+        migrations.CreateModel(
+            name='BlacklistEntry',
+            fields=[
+                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(max_length=75)),
+                ('added', models.DateTimeField(default=django.utils.timezone.now, editable=False)),
+            ],
+            options={
+                u'ordering': ('-added',),
+                u'verbose_name': u'blacklist entry',
+                u'verbose_name_plural': u'blacklist entries',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='MailTemplate',
+            fields=[
+                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name=u'Name')),
+                ('plain', models.TextField(verbose_name=u'Plaintext Body')),
+                ('html', models.TextField(null=True, verbose_name=u'HTML Body', blank=True)),
+                ('subject', models.CharField(max_length=255, verbose_name=u'Subject')),
+            ],
+            options={
+                u'ordering': ('name',),
+                u'verbose_name': u'mail template',
+                u'verbose_name_plural': u'mail templates',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Newsletter',
+            fields=[
+                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name=u'Name')),
+                ('description', models.TextField(null=True, verbose_name=u'Description', blank=True)),
+            ],
+            options={
+                u'ordering': ('name',),
+                u'verbose_name': u'newsletter',
+                u'verbose_name_plural': u'newsletters',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='BounceEntry',
+            fields=[
+                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.CharField(max_length=255, null=True, verbose_name=u'recipient', blank=True)),
+                ('exception', models.TextField(null=True, verbose_name=u'exception', blank=True)),
+            ],
+            options={
+                u'ordering': ('email',),
+                u'verbose_name': u'bounce entry',
+                u'verbose_name_plural': u'bounce entries',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SubscriberList',
+            fields=[
+                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name=u'Name')),
+                ('content_type', models.ForeignKey(to=u'contenttypes.ContentType', to_field=u'id')),
+                ('filter_condition', campaign.fields.JSONField(default='{}', help_text=u'Django ORM compatible lookup kwargs which are used to get the list of objects.')),
+                ('email_field_name', models.CharField(help_text=u'Name of the model field which stores the recipients email address', max_length=64, verbose_name=u'Email-Field name')),
+            ],
+            options={
+                u'ordering': ('name',),
+                u'verbose_name': u'subscriber list',
+                u'verbose_name_plural': u'subscriber lists',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Campaign',
+            fields=[
+                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name=u'Name')),
+                ('newsletter', models.ForeignKey(verbose_name=u'Newsletter', to_field=u'id', blank=True, to='campaign.Newsletter', null=True)),
+                ('template', models.ForeignKey(to='campaign.MailTemplate', to_field=u'id', verbose_name=u'Template')),
+                ('sent', models.BooleanField(default=False, verbose_name=u'sent out', editable=False)),
+                ('sent_at', models.DateTimeField(null=True, verbose_name=u'sent at', blank=True)),
+                ('online', models.BooleanField(default=True, help_text=u'make a copy available online', verbose_name=u'available online')),
+                ('recipients', models.ManyToManyField(to='campaign.SubscriberList', verbose_name=u'Subscriber lists')),
+            ],
+            options={
+                u'ordering': ('name', 'sent'),
+                u'verbose_name': u'campaign',
+                u'verbose_name_plural': u'campaigns',
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- campaign/migrations/__init__.py
+++
@@ -1,2 +0,0 @@
-from south.modelsinspector import add_introspection_rules
-add_introspection_rules([], ["^campaign\.fields\.JSONField"])
